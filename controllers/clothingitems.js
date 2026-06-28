513f457f21555e6c22b50146a4888c09ff1ab851
Nathan Neel <nwneel@hotmail.com>
2026-06-28 16:06:58 -0700

    new update for my project

---
+++ controllers/clothingitems.js
@@ -0,0 +1,50 @@
+const ClothingItem = require("../models/clothingItems");
+
+const createItem = (req, res) => {
+  const { name, weather, imageURL } = req.body;
+
+  ClothingItem.create({ name, weather, imageURL })
+    .then((item) => {
+      res.send({ data: item });
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error from createItem", e });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from getItems", e });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageURL } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from updateItem", e });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from deleteItem", e });
+    });
+};
+
+module.exports = {
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem,
+};
--- models/clothingitems.js
+++ models/clothingitems.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
+const validator = requir("validator");
 
-const clothingItemsSchema = new mongoose.Schema({});
+const clothingItemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weather: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "Link is not valid",
+    },
+  },
+});
 
-module.exports = mongoose.model("item", clothingItemsSchema);
+module.exports = mongoose.model("clothingItems", clothingItem);
